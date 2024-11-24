b1cf90648e7b7554380f3fc6694df179d09eaebe
saraadel6 <101582885+saraadel6@users.noreply.github.com>
2024-11-24 13:49:49 +0200

    Update LogisticReg.py

--- LogisticReg.py
+++ LogisticReg.py
@@ -3,19 +3,23 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
-# 1D Array
+# selected features
+x_train_features = d.X_train[['Engine Size(L)', 'Fuel Consumption City (L/100 km)']]
+x_test_features = d.X_test[['Engine Size(L)', 'Fuel Consumption City (L/100 km)']]
+
+# convert the target to 1d array
 y_train = d.y_train['Emission Class'].to_numpy().ravel()
 y_test = d.y_test['Emission Class'].to_numpy().ravel()
 
 # SGDClassifier
 Logistic_model = SGDClassifier(loss='log_loss', random_state=42, max_iter=1000)
 
-Logistic_model.fit(d.X_train, y_train)
+# Fit
+Logistic_model.fit(x_train_features, y_train)
 
-# Make predictions
-y_predict = Logistic_model.predict(d.X_test)
+# prediction
+y_predict = Logistic_model.predict(x_test_features)
 
-# Calculate accuracy
+# accuracy
 accuracy = accuracy_score(y_test, y_predict)
 print(f"Model Accuracy: {accuracy:.2f}")
-
